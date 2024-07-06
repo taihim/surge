2b69abbb57548b98a1a1ec873d2f517ad9829f40
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-06 16:20:24 +0200

    chore: setup dev tools

---
+++ src/surge/app.py
@@ -0,0 +1,13 @@
+"""Main function for the surge application."""
+
+import asyncio
+
+
+async def main() -> None:
+    """Main function for the surge application."""
+    async with asyncio.TaskGroup():
+        ...
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
