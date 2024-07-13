018829dcf40e76e50fb062c3aa960fcb5ae1e6f0
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-13 23:34:43 +0200

    feat: client updates

--- src/vektor/client.py
+++ src/vektor/client.py
@@ -7,20 +7,13 @@ class HttpClient:
     """An HTTP client that can be used to make requests to a server. Uses the `httpx` AyncClient library."""
 
     _client: AsyncClient = None
-    _instances: dict[str, AsyncClient] = {None}
+    _instances: dict[str, AsyncClient] = None
 
-    def __init__(self, url: str, client_id: uuid4):
+    def __init__(self, base_url: str, headers: dict[str, str] | None = None):
+        self.base_url = base_url
+        self.headers = headers
         self._client = AsyncClient()
-        self._instances[client_id] = self._client
+        self._instances[str(uuid4())] = self._client
 
-    def get(self, path: str, params: Dict[str, str] = None) -> Response:
-        return requests.get(self.url + path, headers=self.headers, params=params)
-
-    def post(self, path: str, data: Dict[str, str] = None) -> Response:
-        return requests.post(self.url + path, headers=self.headers, data=data)
-
-    def put(self, path: str, data: Dict[str, str] = None) -> Response:
-        return requests.put(self.url + path, headers=self.headers, data=data)
-
-    def delete(self, path: str) -> Response:
-        return requests.delete(self.url + path, headers=self.headers)
+    def get(self, path: str, params: dict[str, str] | None = None) -> None:
+        self._client.get(self.base_url + path, headers=self.headers, params=params)
