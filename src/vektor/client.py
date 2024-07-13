cb5b165da7e21f7f7550fc2012e71a5d650f4e8d
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-13 23:58:56 +0200

    feat: updated singleton logic in client

--- src/vektor/client.py
+++ src/vektor/client.py
@@ -1,3 +1,4 @@
+from typing import ClassVar
 from uuid import uuid4
 
 from httpx import AsyncClient
@@ -7,7 +8,7 @@ class VektorClient:
     """An HTTP client that can be used to make requests to a server. Uses the `httpx` AyncClient library."""
 
     _client: AsyncClient = None
-    _instance: "VektorClient" = None
+    _instances: ClassVar[dict[str, "VektorClient"]] = {}
 
     def __init__(self, base_url: str, headers: dict[str, str] | None = None, timeout: float = 5.0) -> None:
         """Initializes the client with the base URL and headers.
@@ -24,10 +25,12 @@ class VektorClient:
     @classmethod
     def get_or_create_instance(cls, base_url: str | None = None, headers: dict[str, str] | None = None, timeout: float = 5.0) -> "VektorClient":
         """Gets or creates an instance of the client."""
-        if not cls._instance:
-            cls._instance = VektorClient(base_url, headers, timeout)
+        client_id = str(uuid4())
 
-        return cls._instance
+        if client_id not in cls._instances:
+            cls._instances[client_id] = VektorClient(base_url, headers, timeout)
+
+        return cls._instances[client_id]
 
     def get(self, path: str, query_params: dict[str, str] | None = None) -> None:
         """Makes a GET request to the server.
